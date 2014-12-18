8ff9b5f74a6c0392e2cf5f066742b80a927b47d8
Joachim Fasting <joachifm@fastmail.fm>
2014-12-18 03:19:10 +0100

    Set power/control attribute via udev

--- nixos/modules/tasks/pci-device-power-management.nix
+++ nixos/modules/tasks/pci-device-power-management.nix
@@ -5,23 +5,9 @@
   #### implementation
 
   config = lib.mkIf config.powerManagement.enable {
-
-    systemd.services."pci-device-pm" = {
-      description = "Enable automatic power management for coldplugged PCI devices.";
-
-      wantedBy = [ "multi-user.target" ];
-      requires = [ "systemd-udev-trigger.service" ];
-
-      script = ''
-        shopt -s nullglob
-        for x in /sys/bus/pci/devices/*/power/control ; do echo -n auto >$x ; done
-      '';
-
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = "yes";
-      };
-
-    };
+    services.udev.extraRules = ''
+      SUBSYSTEM=="pci", ACTION=="add", ATTR{power/control}="auto"
+    '';
   };
+
 }
