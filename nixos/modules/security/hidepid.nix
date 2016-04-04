01cfeca3141abdae44740857e072f9ed12e5c1ac
Joachim Fasting <joachifm@fastmail.fm>
2016-04-04 13:39:01 +0200

    hidepid: fix syntax error & improve description

--- nixos/modules/security/hidepid.nix
+++ nixos/modules/security/hidepid.nix
@@ -4,22 +4,11 @@ with lib;
 {
   options = {
     security.hideProcessInformation = {
-      enable = mkEnableOption = ''
-        restrict access to process information to the process owner.  Enabling
-        this option implies, among other things, that users can can only see
-        their own processes and that information such as command-line arguments
-        is not leaked between users.
-
-        Note that a user may still discover which programs or PIDs are in use
-        with <literal>pkill</literal> and <literal>kill</literal>
-        (a permission denied error would indicate that the program or PID exists),
-        but this requires much more work and provides less useful information than
-        if process information is readily available.
-
-        This option is a more lenient alternative to the <literal>/proc</literal>
-        hardening provided by GRsecurity. Enabling this option is recommended
-        unless you have a legitimate need for allowing unprivileged users to inspect
-        process information owned by other users.
+      enable = mkEnableOption ''restricted access to process information.
+	Enable this option to minimize what unprivileged users can
+	learn about the processes of other users.  In particular, this
+	option ensures that potentially sensitive information passed
+	via command-line arguments remain private.
       '';
     };
   };
