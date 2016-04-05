53ea2f2982dc615c0aaf18abef84d3ffdb451a27
Joachim Fasting <joachifm@fastmail.fm>
2016-04-06 01:24:13 +0200

    hidepid: more description fixups

--- nixos/modules/security/hidepid.nix
+++ nixos/modules/security/hidepid.nix
@@ -4,19 +4,14 @@ with lib;
 {
   options = {
     security.hideProcessInformation = {
-      enable = mkEnableOption ''restricted access to process information to the
-        owning user.  Enable this option to minimize what unprivileged users can
-        learn about the processes of other users.  In particular, this option
-        ensures that potentially sensitive information passed via command-line
-        arguments remain private.
-
-        This is a more compatible (less likely to break legitimate applications)
-        alternative to GRsecurity's <literal>/proc</literal> hardening.
-
-        Enabling this option is recommended for most systems, unless there's a
-        legitimate reason for allowing unprivileged users to inspect the process
-        information about other users.
+      enable = mkEnableOption "" // { description = ''
+        Restrict access to process information to the owning user.  Enabling
+        this option means, among other things, that command-line arguments
+        remain private.  This option is recommended for most systems, unless
+        there's a legitimate reason for allowing unprivileged users to inspect
+        the process information of other users.
       '';
+      };
     };
   };
 
