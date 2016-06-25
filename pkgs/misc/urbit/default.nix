5383401ad36a918bc317327ef8ec82d1f374c065
Joachim Fasting <joachifm@fastmail.fm>
2016-06-25 15:08:34 +0200

    urbit: support bootstrap
    urbit can use a pre-compiled Arvo binary to bootstrap itself.  Unfortunately,
    urbit expects the binary to be in the current working directory, where it also
    will write state.  To work around this, generate a wrapper that cds into
    URBIT_HOME, copying the pre-compiled arvo binary into it if necessary, before
    calling out to the real urbit.  This seems to work for me at least.

--- pkgs/misc/urbit/default.nix
+++ pkgs/misc/urbit/default.nix
@@ -1,8 +1,18 @@
-{ stdenv, fetchFromGitHub, gcc, gmp, libsigsegv, openssl, automake, autoconf, ragel,
-  cmake, re2c, libtool, ncurses, perl, zlib, python }:
+{ stdenv, fetchurl, fetchFromGitHub
+, automake, autoconf, libtool, pkgconfig, cmake
+, ragel, re2c, perl
+, gmp, libsigsegv, openssl, zlib
+, ncurses
+}:
+
+let
+  arvoBoot = fetchurl {
+    url = "https://bootstrap.urbit.org/latest.pill";
+    sha256 = "02508ixs85x2j66y5yvikj6zarim1x9byz2nmsd63z0bvykl6i5z";
+  };
+in
 
 stdenv.mkDerivation rec {
-
   name = "urbit-${version}";
   version = "2016-06-02";
 
@@ -13,31 +23,45 @@ stdenv.mkDerivation rec {
     sha256 = "055qdpp4gm0v04pddq4380pdsi0gp2ybgv1d2lchkhwsnjyl46jl";
   };
 
-  buildInputs = with stdenv.lib; [
-    gcc gmp libsigsegv openssl automake autoconf ragel cmake re2c libtool
-    ncurses perl zlib python
-  ];
+  nativeBuildInputs = [ automake autoconf libtool cmake pkgconfig ragel re2c perl ];
 
-  configurePhase = ''
-    :
+  buildInputs = [ gmp libsigsegv openssl zlib ]
+    ++ stdenv.lib.optional stdenv.isLinux ncurses;
+
+  postPatch = stdenv.lib.optionalString stdenv.isLinux ''
+    substituteInPlace Makefile --replace "-lcurses" "-lncurses"
   '';
 
-  buildPhase = ''
-    sed -i 's/-lcurses/-lncurses/' Makefile
-    mkdir -p $out
-    cp -r . $out/
-    cd $out
-    make
-  '';
+  # the build invokes cmake indirectly; in a way that is incompatible with
+  # cmake's setup-hook
+  configurePhase = ":";
 
   installPhase = ''
-    :
+    mkdir -p $out/bin
+    cp -v bin/urbit $out/bin/.urbit-wrapped
+
+    cat >$out/bin/urbit <<EOF
+    #! $shell -e
+    if [ -z "\$URBIT_HOME" ] ; then
+      URBIT_HOME=\''${XDG_DATA_HOME:-\$HOME/.local/share}/urbit
+    fi
+    if [ ! -d "\$URBIT_HOME" ] ; then
+      mkdir -pv "\$URBIT_HOME"
+    fi
+    if [ ! -f "\$URBIT_HOME"/urbit.pill ] || [ "\$URBIT_HOME"/urbit.pill -ot ${arvoBoot} ] ; then
+      cp -fv ${arvoBoot} "\$URBIT_HOME"/urbit.pill
+    fi
+    cd "\$URBIT_HOME"
+    exec -a $out/bin/urbit $out/bin/.urbit-wrapped "\$@"
+    EOF
+    chmod +x $out/bin/urbit
   '';
 
   meta = with stdenv.lib; {
     description = "An operating function";
     homepage = http://urbit.org;
     license = licenses.mit;
+    platforms = platforms.unix;
     maintainers = with maintainers; [ mudri ];
   };
 }
