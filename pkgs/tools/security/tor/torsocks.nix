295989918de2168d6cbccb9eec104223174f8308
Joachim Fasting <joachifm@fastmail.fm>
2016-12-04 18:58:12 +0100

    torsocks: redo patching, ensure getcap is available

--- pkgs/tools/security/tor/torsocks.nix
+++ pkgs/tools/security/tor/torsocks.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchgit, autoreconfHook, which }:
+{ stdenv, fetchgit, autoreconfHook, libcap }:
 
 stdenv.mkDerivation rec {
   name = "torsocks-${version}";
@@ -12,9 +12,12 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ autoreconfHook ];
 
-  patchPhase = ''
-    substituteInPlace src/bin/torsocks.in \
-      --replace which ${which}/bin/which
+  postPatch = ''
+    # Patch torify_app()
+    sed -i \
+	-e 's,local app_path=`which $1`,local app_path=`type -P $1`,' \
+        -e 's,local getcap=.*,local getcap=${libcap}/bin/getcap,' \
+	src/bin/torsocks.in
   '';
 
   meta = {
