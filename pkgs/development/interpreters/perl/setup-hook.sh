fded5b7f65b6fa58bdfb954585b2f37695f35a9a
Joachim Fasting <joachifm@fastmail.fm>
2017-01-01 04:43:47 +0100

    perl: deterministic hash seed in build env
    Setting PERL_HASH_SEED=0 ensures that all runs process dictionary keys
    in the same order.  Obviously, this is insecure but makes sense within
    the build environment, where, presumably, algorithm complexity attacks
    are irrelevant.
    
    See http://perldoc.perl.org/perlrun.html#ENVIRONMENT
    and https://reproducible-builds.org/docs/stable-outputs/

--- pkgs/development/interpreters/perl/setup-hook.sh
+++ pkgs/development/interpreters/perl/setup-hook.sh
@@ -1,5 +1,9 @@
+setDeterministicPerlHashSeed() {
+    export PERL_HASH_SEED=0 # implies PERL_PERTURB_KEYS=0
+}
+
 addPerlLibPath () {
     addToSearchPath PERL5LIB $1/lib/perl5/site_perl
 }
 
-envHooks+=(addPerlLibPath)
+envHooks+=(setDeterministicPerlHashSeed addPerlLibPath)
