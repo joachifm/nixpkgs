0f04dac77b914b865e9e408556aecddb0b600370
Joachim Fasting <joachifm@fastmail.fm>
2017-06-23 00:04:58 +0200

    nixos/xl2tpd: replace uses of types.string

--- nixos/modules/services/networking/xl2tpd.nix
+++ nixos/modules/services/networking/xl2tpd.nix
@@ -8,13 +8,13 @@ with lib;
       enable = mkEnableOption "Whether xl2tpd should be run on startup.";
 
       serverIp = mkOption {
-        type        = types.string;
+        type        = types.str;
         description = "The server-side IP address.";
         default     = "10.125.125.1";
       };
 
       clientIpRange = mkOption {
-        type        = types.string;
+        type        = types.str;
         description = "The range from which client IPs are drawn.";
         default     = "10.125.125.2-11";
       };
