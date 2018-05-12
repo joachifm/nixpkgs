b9f412a543d74a0bf2a8050ff06cef5e57e54582
Joachim Fasting <joachifm@fastmail.fm>
2018-05-12 20:46:10 +0200

    closureInfo: use runCommand

--- pkgs/build-support/closure-info.nix
+++ pkgs/build-support/closure-info.nix
@@ -4,31 +4,25 @@
 # "nix-store --load-db" and "nix-store --register-validity
 # --hash-given".
 
-{ stdenv, coreutils, jq }:
+{ runCommand, coreutils, jq }:
 
 { rootPaths }:
 
 assert builtins.langVersion >= 5;
 
-stdenv.mkDerivation {
-  name = "closure-info";
-
-  __structuredAttrs = true;
-
+runCommand "closure-info"
+{ __structuredAttrs = true;
   exportReferencesGraph.closure = rootPaths;
-
-  PATH = "${coreutils}/bin:${jq}/bin";
-
-  builder = builtins.toFile "builder"
-    ''
-      . .attrs.sh
-
-      out=''${outputs[out]}
-
-      mkdir $out
-
-      jq -r ".closure | map(.narSize) | add" < .attrs.json > $out/total-nar-size
-      jq -r '.closure | map([.path, .narHash, .narSize, "", (.references | length)] + .references) | add | map("\(.)\n") | add' < .attrs.json | head -n -1 > $out/registration
-      jq -r .closure[].path < .attrs.json > $out/store-paths
-    '';
+  nativeBuildInputs = [ coreutils jq ];
+  preferLocalBuild = true;
+  allowSubstitutes = false;
 }
+''
+. .attrs.sh
+out=''${outputs[out]}
+mkdir $out
+
+jq -r ".closure | map(.narSize) | add" < .attrs.json > $out/total-nar-size
+jq -r '.closure | map([.path, .narHash, .narSize, "", (.references | length)] + .references) | add | map("\(.)\n") | add' < .attrs.json | head -n -1 > $out/registration
+jq -r .closure[].path < .attrs.json > $out/store-paths
+''
